380a1d4337ed7e77cbe1e5eae22747ddf3cbe59e
meap02 <79549780+meap02@users.noreply.github.com>
2022-03-23 10:57:39 -0700

    Designed the !assign function and error handling
    Redesigned the post function, one will be callable through discord, one 
    will be activated once a day

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 # bot.py
 
-import os
 import discord
 from discord.ext import commands
 import configparser
@@ -24,54 +23,23 @@ TOKEN = config["default"]["BOT_TOKEN"]
 admin_user = config["default"]["admin_user"]
 with open("servers.json", "r") as f:
     servers = json.load(f)
-
 ###############################################################################
 #                               Message Prompts
 ###############################################################################
-introduction = """Hey! I'm DailyBot, created by Kyle Just. I can do a various amount of tasks but my main one is to fetch random posts from reddit and send them here daily. First things first though,I will need a channel to inhabit and send my messages
-                 in, could you use the "!assign {text channel}" command and I
-                 will use that channel from now on. If not, I could just go
-                 ahead and keep using this one. One more thing, if you want to
-                 know more about what I can do, just use the "!help" command.
-                 Thank you for letting me be a part of your server <3"""
+introduction = """Hey! I'm DailyBot, created by Kyle Just. I can do a various
+amount of tasks but my main one is to fetch random posts from reddit and send
+them here daily. First things first though, I will need a channel to inhabit
+and send my messages in, could you use the "!assign {text channel}" command and
+I will use that channel from now on. If not, I could just go ahead and keep
+using this one. One more thing, if you want to know more about what I can do,
+just use the "!help" command. Thank you for letting me be a part of your server
+<3""".replace("\n", " ")
 ###############################################################################
 #                               GLOBALS
 ###############################################################################
 bot = commands.Bot(command_prefix='!')
-WHEN = time(15, 23, 0)  # 6:00 PM
+WHEN = time(1, 24, 30)  # 6:00 PM
 ###############################################################################
-
-
-@bot.command()
-async def post(ctx):  # Fired every day
-    # Make sure your guild cache is ready so the channel can be found via get_channel
-    await bot.wait_until_ready()
-    # change this to adress dictionary
-    channel = ctx.guild.get_channel(channel_home)
-    print("The one-a-day has been triggered")
-    await channel.send("Booya!")  # REDDIT POST WILL GO HERE
-
-
-@bot.command(name="assign")
-async def addChannelHome(ctx, home):
-    # Make sure your guild cache is ready so the channel can be found via get_channel
-    await bot.wait_until_ready()
-    if(home):
-        return
-    print("The test function has been activated")
-
-
-@addChannelHome.error
-async def addChannelHome_error(ctx, error):
-    await ctx.send("There were not enough arguments to assign me to a channel")
-    pass
-
-
-@bot.command()
-async def shutdown(ctx):
-    if(ctx.author == admin_user):
-        bot.destroy()
-        exit(self)
 
 
 @bot.event
@@ -85,11 +53,48 @@ async def on_ready():
           + str([o.name for o in bot.guilds]))
     bot.loop.create_task(background_task())
     for guild in bot.guilds:
-        if guild.name in servers:
+        if str(guild.id) in servers:
             pass
         else:
-            servers[guild.name] = {guild.id: guild.text_channels[0]}
-            await guild.text_channels[0].send(content=introduction)
+            servers[guild.id] = guild.text_channels[0].id
+            await guild.text_channels[0].\
+                send(content=introduction)
+
+
+async def post():  # Fired every day
+    await bot.wait_until_ready()
+    # DUPLICATE THE METHOD, ONE FOR REQUESTED POST, THE OTHER FOR DAILY POST
+    print("The one-a-day has been triggered")
+    for guild in servers:
+        bot.get_guild(int(guild)).get_channel(servers[guild]).send("Booya!")
+        # REDDIT POST WILL GO HERE
+
+
+@bot.command()
+async def assign(ctx, home):
+    await bot.wait_until_ready()
+    names = [o.name for o in ctx.guild.text_channels]
+    home = home.replace("#", "")
+    if home in names:
+        channel = ctx.guild.text_channels[names.index(home)]
+        servers[str(ctx.guild.id)] = channel.id
+        await ctx.guild.get_channel(channel.id).\
+            send("Thank you for the new home!")
+    else:
+        await ctx.\
+            send("I don't think that channel exists, is it spelled correctly?")
+
+
+@assign.error
+async def assign_error(ctx, error):
+    await ctx.send("There were not enough arguments to assign me to a channel :(")
+
+
+@bot.command(aliases=["sd", "exit"])
+async def shutdown(ctx):
+    if(str(ctx.author) == admin_user):
+        print("Shutdown signal given...")
+        await bot.close()  # STOP BOT
 
 
 @bot.event
