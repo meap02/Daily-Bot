7d677c2d93a6c0926c952e1e37295f8e5ddd7927
meap02 <79549780+meap02@users.noreply.github.com>
2022-03-25 02:39:42 -0700

    Finished the add and remove functions with proper error handling
    Random function has been implemented, now to work on extracting data 
    from reddit post to then post to discord
    
    Isolated the saving procedures to its own function
    
    Extarct function can succesfully identify the media in all reddit posts

--- bot.py
+++ bot.py
@@ -8,6 +8,9 @@ import asyncio
 import json
 import re
 import asyncpraw
+import asyncprawcore
+from pprint import pprint
+import random
 
 
 ###############################################################################
@@ -28,16 +31,16 @@ admin_user = config["DISCORD"]["admin_user"]
 
 ###REDDIT###
 reddit = asyncpraw.Reddit(
-    client_id = config["REDDIT"]["client_id"],
-    client_secret = config["REDDIT"]["client_secret"],
-    user_agent = config["REDDIT"]["user_agent"]
+    client_id=config["REDDIT"]["client_id"],
+    client_secret=config["REDDIT"]["client_secret"],
+    user_agent=config["REDDIT"]["user_agent"]
 )
 ###LOCAL FILES###
 with open("servers.json", "r") as f:
     servers = json.load(f)
 with open("reddit.json", "r") as f:
     feed = json.load(f)
-feed = {"954653107734851645": ["shitposting", "mildlyinfuriating"]}
+#feed = {"954653107734851645": ["shitposting", "mildlyinfuriating"]}
 ###############################################################################
 #                               Message Prompts
 ###############################################################################
@@ -51,6 +54,13 @@ just use the "!help" command. Thank you for letting me be a part of your server
 <3""".replace("\n", " ")
 home_thank = "Thank you for the new home!"
 channel_dne = "I don't think that channel exists, is it spelled correctly?"
+subreddit_dne = "I don't think that subreddit exists, is it spelled correctly?"
+subreddit_add = "The subreddit \"{}\" has been added to the feed!"
+subreddit_exist = "The subreddit \"{}\" has already been added!"
+assign_error_s = "There were not enough arguments to assign me to a channel :("
+add_error_s = "There were not enough arguments to add anything to the feed :/"
+subreddit_rm = "The subreddit \"{}\" has been removed from the feed"
+subreddit_rm_error = "\"{}\" is not a subreddit in the current feed"
 ###############################################################################
 #                               GLOBALS
 ###############################################################################
@@ -95,13 +105,52 @@ async def post():  # Fired every day
 
 
 @bot.command()
-async def add(ctx, new): #Will add a specidic subreddit or user to the selected pool
+async def add(ctx, new):  # Will add a specidic subreddit or user to the selected pool
     await bot.wait_until_ready()
     if ctx.channel.id != servers[str(ctx.guild.id)]:
         return
-    await subject = reddit.subreddit(new)
-    print(subject.display_name)
-    feed[str(ctx.guild.id)].append(subject.id)
+    try:
+        subject = await reddit.subreddit(new, fetch=True)
+        if subject.display_name in feed[str(ctx.guild.id)]:
+            await ctx.send(subreddit_exist.format(new))
+        else:
+            feed[str(ctx.guild.id)].append(subject.display_name)
+            print("{0.user} has added ".format(bot)
+                  + new + " to the feed in " + str(ctx.guild))
+            await ctx.send(subreddit_add.format(new))
+    except asyncprawcore.exceptions.Redirect as e:
+        await ctx.send(subreddit_dne)
+
+
+@add.error
+async def add_error(ctx, error):
+    if ctx.channel.id != servers[str(ctx.guild.id)]:
+        return
+    await ctx.send(add_error_s)
+
+
+@bot.command(aliases= ["rm"])
+async def remove(ctx, rid):
+    await bot.wait_until_ready()
+    if ctx.channel.id != servers[str(ctx.guild.id)]:
+        return
+    list = feed[str(ctx.guild.id)]
+    if rid in list:
+        del list[list.index(rid)]
+        await ctx.send(subreddit_rm.format(rid))
+    else:
+        await ctx.send(subreddit_rm_error.format(rid))
+
+
+@bot.command(name="random")
+async def randomPost(ctx):
+    await bot.wait_until_ready()
+    if ctx.channel.id != servers[str(ctx.guild.id)]:
+        return
+    list = feed[str(ctx.guild.id)]
+    for i in range(0, 4):
+        sub = await reddit.subreddit(random.choice(list))
+        media = extract(await sub.random())
 
 
 @bot.command()
@@ -113,6 +162,8 @@ async def assign(ctx, home):
     if home in names:
         channel = ctx.guild.text_channels[names.index(home)]
         servers[str(ctx.guild.id)] = channel.id
+        print("{0.user} has been assigned to ".format(bot) + home)
+
         await ctx.guild.get_channel(channel.id).send(home_thank)
         return
     try:
@@ -121,8 +172,8 @@ async def assign(ctx, home):
         ids = [o.id for o in ctx.guild.text_channels]
         if home in ids:
             channel = ctx.guild.get_channel(home)
-            print("{0.user} has been assigned to ".format(bot) + channel.name)
             servers[str(ctx.guild.id)] = home
+            print("{0.user} has been assigned to ".format(bot) + channel.name)
             await channel.send(home_thank)
             return
     except ValueError:
@@ -132,8 +183,9 @@ async def assign(ctx, home):
 
 @assign.error
 async def assign_error(ctx, error):
-    await ctx.\
-        send("There were not enough arguments to assign me to a channel :(")
+    if ctx.channel.id != servers[str(ctx.guild.id)]:
+        return
+    await ctx.send(assign_error_s)
 
 
 @bot.command(aliases=["sd", "exit"])
@@ -144,10 +196,52 @@ async def shutdown(ctx):
         print("Shutdown signal given...")
         await bot.close()  # STOP BOT
 
+@bot.command(name="save")
+async def CalledSave(ctx):
+    await bot.wait_until_ready()
+    if ctx.channel.id != servers[str(ctx.guild.id)]:
+        return
+    if(str(ctx.author) == admin_user):
+        await ctx.send("Saving data...")
+        save()
+        print("Saved data")
 
 @bot.event
 async def on_disconnect():
     print("The bot has been disconnected, saving necessary data...")
+    save()
+
+
+@bot.event
+async def on_error(event):
+    on_disconnect()
+    sys.exc_info()
+
+
+def extract(post):
+    try:
+        match post.post_hint:
+            case 'image':
+                print("This is an image: https://www.reddit.com" + post.permalink)
+            case 'hosted:video':
+                print(
+                    "This is a video on reddit: https://www.reddit.com" + post.permalink)
+            case 'rich:video':
+                print("This is a rich video: https://www.reddit.com" + post.permalink)
+            case _:
+                print(
+                    "I'm not sure what this is: https://www.reddit.com" + post.permalink + " " + post.post_hint)
+    except:
+        if "https://www.reddit.com" + post.permalink == post.url:
+            print("This post is text: https://www.reddit.com" + post.permalink)
+        elif (re.search("/www\.reddit\.com/gallery", post.url) != None):
+            print("This is a series of images on reddit: https://www.reddit.com" + post.permalink)
+        else:
+            print(
+                "This post does not have a hint: https://www.reddit.com" + post.permalink)
+            print(post.url)
+
+def save():
     with open("servers.json", "w") as f:
         json.dump(servers, f)
     with open("reddit.json", "w") as f:
